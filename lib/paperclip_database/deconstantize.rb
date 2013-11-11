059530652d6fb0d52946fc4f8c92421f5214692f
Charles Pence <charles@charlespence.net>
2013-11-11 15:15:56 -0500

    Use Rails' deconstantize method if available.

--- lib/paperclip_database/deconstantize.rb
+++ lib/paperclip_database/deconstantize.rb
@@ -2,6 +2,10 @@
 module PaperclipDatabase
   # This function only appears in Rails >= 3.2.1, so define our own copy here
   def self.deconstantize(path)
-    path.to_s[0...(path.rindex('::') || 0)]
+    if path.respond_to? :deconstantize
+      path.deconstantize
+    else
+      path.to_s[0...(path.rindex('::') || 0)]
+    end
   end
 end
