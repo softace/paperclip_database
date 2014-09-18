12c0a1075d530b6c1c6a359d113156485bd52af6
Jarl Friis <jarl@softace.dk>
2014-09-18 10:57:45 +0200

    Adding test cases

--- spec/major_version_API_spec.rb
+++ spec/major_version_API_spec.rb
@@ -35,7 +35,7 @@ describe "PaperclipDatabase" do
         reset_activerecord
         reset_database :users, :avatars
       end
-      it_behaves_like "major version API compatible", :table_name => 'avatars', :association_name => 'user_avatar_paperclip_files'
+      it_behaves_like "major version API compatible", :table_name => 'avatars'
     end
     describe "with custom model table_name" do
       before(:context) do
@@ -52,11 +52,41 @@ describe "PaperclipDatabase" do
         reset_activerecord
         reset_database :custom_users, :avatars
       end
-      it_behaves_like "major version API compatible", :table_name => 'avatars', :association_name => 'c_user_avatar_paperclip_files'
+      it_behaves_like "major version API compatible", :table_name => 'avatars'
     end
     describe "with custom attachment table_name" do
+      before(:context) do
+        create_model_tables :a_users, :custom_avatars, 'avatar'
+        build_model 'AUser', nil, :avatar, {:database_table => :custom_avatars}
+        @model = AUser.new
+        file = File.open(fixture_file('5k.png'))
+
+        @model.avatar = file
+        @model.save
+      end
+      subject(:association){@model.avatar}
+      after(:context) do
+        reset_activerecord
+        reset_database :a_users, :custom_avatars
+      end
+      it_behaves_like "major version API compatible", :table_name => 'custom_avatars'
     end
     describe "with custom model table_name and attachment table_name" do
+      before(:context) do
+        create_model_tables :custom_users, :custom_avatars, 'avatar'
+        build_model 'CaUser', 'custom_users', :avatar, {:database_table => :custom_avatars}
+        @model = CaUser.new
+        file = File.open(fixture_file('5k.png'))
+
+        @model.avatar = file
+        @model.save
+      end
+      subject(:association){@model.avatar}
+      after(:context) do
+        reset_activerecord
+        reset_database :custom_users, :custom_avatars
+      end
+      it_behaves_like "major version API compatible", :table_name => 'custom_avatars'
     end
   end
 end
